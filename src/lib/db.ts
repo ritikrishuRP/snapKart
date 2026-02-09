e9155fd2f70ab308345c5bc553e7a190f0a8d25b
rishu <rrishu212@gmail.com>
2026-02-09 18:11:56 +0530

    Initial setup with models and db config

---
+++ src/lib/db.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+
+const mongodbUrl=process.env.MONGODB_URL
+
+if(!mongodbUrl){
+    throw new Error("MONGODB_URL is not defined in environment variables")
+}
+
+
+let cached = global.mongoose
+
+if(!cached){
+    cached= global.mongoose={conn:null,promise:null}
+}
+
+const connectDB = async ()=>{
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.promise){
+        cached.promise=mongoose.connect(mongodbUrl).then((conn)=>conn.connection)
+    }
+
+    try {
+        const conn = await cached.promise
+        return conn
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export default connectDB;
+
